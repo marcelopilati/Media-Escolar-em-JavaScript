f0a278003cc6f724a266c88a8726586af08d919f
Marcelo Pilati <109703603+marcelopilati@users.noreply.github.com>
2023-01-05 17:54:25 -0300

    Update Media Escolar em JavaScript.js

--- Media Escolar em JavaScript.js
+++ Media Escolar em JavaScript.js
@@ -2,16 +2,16 @@ import input from 'readline-sync';
 
 console.log("Media Escolar! \n");
 
-var nota1 = input.question("Insira a primeira nota: ");
-var nota2 = input.question("Insira a segunda nota: ");
-var nota3 = input.question("Insira a terceira nota: ");
+var nota1 = input.question("Insira a nota referente ao primeiro trimestre: ");
+var nota2 = input.question("Insira a nota referente ao segundo trimestre: ");
+var nota3 = input.question("Insira a nota referente ao terceiro trimestre: ");
 
 var soma_notas = Number(nota1) + Number(nota2) + Number(nota3);
 var media = soma_notas / 3;
 
 console.log("Sua media e: "+media+"");
 
-if(media > 6){
+if(media >= 6){
     console.log("Aprovado!");
 }
 else 
